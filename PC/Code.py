a0b2d97f472f5168ab7fea997a9fd93afc4fff76
eungaaee <aidjiwon1004@gmail.com>
2024-11-19 23:54:04 +0900

    1. 컴퓨터 측에서 기상청 API로 강수 확률이 일정 수치를 넘는지, 넘지 않는지 확인하는 함수 구현, 2. 컴퓨터 측 파이썬 코드를 클래스 기반으로 리팩토링

--- PC/Code.py
+++ PC/Code.py
@@ -2,20 +2,111 @@ import os
 import serial
 import time
 
+import requests
+import datetime
+import json
+
 os.system("cls")
 
-arduino = serial.Serial(port="COM5", baudrate=9600, timeout=0.1)
+class Alarm:
+    def __init__(self, arduino):
+        self.arduino = arduino
 
-def send_initial_time():
-    current_time = time.strftime("%H %M") # 공백으로 구분해서 보내고 아두이노에서 parseInt로 분리
-    print(f"Sending : {current_time}")
-    arduino.write(bytes(current_time, 'utf-8'))
+    def send_initial_time(self):
+        current_time = time.strftime("%H %M") # 공백으로 구분해서 보내고 아두이노에서 parseInt로 분리
+        print(f"Sending : {current_time}")
+        self.arduino.write(bytes(current_time, 'utf-8'))
 
-    time.sleep(0.5) # 모든 바이트가 전송되었을 때 까지 기다리기
-    received = arduino.readline().decode('utf-8')
-    print(f"received : {received}")
+        time.sleep(0.5) # 모든 바이트가 전송되었을 때 까지 기다리기
+        received = self.arduino.readline().decode('utf-8')
+        print(f"Received : {received}")
 
-while (input("init? : ") == 'y'): # 입력 하고 너무 빠르게 엔터를 누르면 안 됨
-    send_initial_time()
-    
-arduino.close()
+class Umbrella:
+    BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
+
+    def __init__(self, arduino, API_KEY, NX, NY):
+        self.arduino = arduino
+        self.API_KEY = self.load_api_key()
+        self.NX, self.NY = NX, NY # 상도1동
+
+    def load_api_key(self):
+        script_dir = os.path.dirname(__file__)
+        with open(os.path.join(script_dir, "api_key.json"), "r") as file:
+            data = json.load(file)
+            return data['KEY']
+
+    def get_latest_base_time(self, now):
+        forecast_times = ["0200", "0500", "0800", "1100", "1400", "1700", "2000", "2300"]
+        
+        for time in reversed(forecast_times):
+            if now.strftime("%H%M") >= time:
+                return time
+            
+        # 만약 현재 시간이 0200 이전이면, 가장 마지막 발표 시간인 2300으로 설정
+        return forecast_times[-1]
+
+    def will_it_rain(self):
+        now = datetime.datetime.now()
+
+        if now.hour < 5:
+            base_date = (now - datetime.timedelta(days=1)).strftime("%Y%m%d")  # 어제 날짜
+        else:
+            base_date = now.strftime("%Y%m%d")  # 오늘 날짜
+        
+        base_time = self.get_latest_base_time(now)
+
+        params = {
+            "serviceKey": self.API_KEY,
+            "numOfRows": "1000",
+            "pageNo": "1",
+            "dataType": "JSON",
+            "base_date": base_date,
+            "base_time": base_time,
+            "nx": self.NX,
+            "ny": self.NY,
+        }
+
+        try:
+            response = requests.get(self.BASE_URL, params=params, timeout=10)
+            response.raise_for_status()
+            data = response.json()
+
+            try:
+                items = data['response']['body']['items']['item']
+                rain_prob_threshold = 50 # 기준 강수확률
+
+                for item in items:
+                    if item['category'] == 'POP':
+                        fcst_time = int(item['fcstTime'])
+                        fcst_prob = int(item['fcstValue'])
+                        if 600 <= fcst_time <= 2100 and fcst_prob >= rain_prob_threshold:
+                            return True
+
+                return False
+            except KeyError:
+                raise KeyError("JSON 데이터 구조 오류")
+        except requests.exceptions.RequestException as e:
+            raise ConnectionError("네트워크 또는 API 요청 오류:", e)
+        except ValueError:
+            raise ValueError("JSON 응답이 비어있거나 형식이 잘못되었습니다.")
+
+def main():
+    arduino = serial.Serial(port="COM5", baudrate=9600, timeout=0.1)
+
+    alarm = Alarm(arduino)
+    umbrella = Umbrella(arduino=arduino, API_KEY = "make your own ./api_key.json", NX=59, NY=125)
+
+    if (input("Enter 'y' after setting up Arduino: ") == 'y'): # 입력 하고 너무 빠르게 엔터를 누르면 안 됨
+        alarm.send_initial_time()
+
+    if (input("Enter 'y' to check the weather: ") == 'y'):
+        try:
+            if umbrella.will_it_rain():
+                print("비가 내릴 수 있습니다.")
+            else:
+                print("비가 내리지 않을 것입니다.")
+        except Exception as e:
+            print(f"error occured: {e}")
+
+if __name__ == "__main__":
+    main()
--- wakeupbrella.ino
+++ wakeupbrella.ino
@@ -1,7 +1,7 @@
 // 아두이노->컴퓨터로 시리얼 통신 할 때 Serial.println() 사용하면 오류 발생. Serial.print() 사용하기
-#include <time.h>
 #include <IRremote.h>
-#define DEBUG
+#include <time.h>
+#define DEBUG_MODE
 
 // remote
 int recvPin = 7;
@@ -10,8 +10,8 @@ IRrecv irrecv(recvPin);
 
 // alarm
 struct {
-  int hour;
-  int minute;
+    int hour;
+    int minute;
 } cur_time = {-1, -1};
 int hhmm[4], hhmm_idx;
 bool is_alarm_setting = false;
@@ -25,124 +25,125 @@ int AB = 9;
 int led_pin = 8;
 
 int result_to_int(long unsigned int res) {
-  int num;
-  if (res == 0xFF6897) num = 0;
-  else if (res == 0xFF30CF) num = 1;
-  else if (res == 0xFF18E7) num = 2;
-  else if (res == 0xFF7A85) num = 3;
-  else if (res == 0xFF10EF) num = 4;
-  else if (res == 0xFF38C7) num = 5;
-  else if (res == 0xFF5AA5) num = 6;
-  else if (res == 0xFF42BD) num = 7;
-  else if (res == 0xFF4AB5) num = 8;
-  else if (res == 0xFF52AD) num = 9;
+    int num;
+    
+    if (res == 0xFF6897) num = 0;
+    else if (res == 0xFF30CF) num = 1;
+    else if (res == 0xFF18E7) num = 2;
+    else if (res == 0xFF7A85) num = 3;
+    else if (res == 0xFF10EF) num = 4;
+    else if (res == 0xFF38C7) num = 5;
+    else if (res == 0xFF5AA5) num = 6;
+    else if (res == 0xFF42BD) num = 7;
+    else if (res == 0xFF4AB5) num = 8;
+    else if (res == 0xFF52AD) num = 9;
 
-  return num;
+    return num;
 }
 
 void set_alarm() {
-  if (irrecv.decode(&result)) {
-    switch (result.value) {
-      case 0xFF906F: // EQ
-        is_alarm_setting = !is_alarm_setting;
-        break;
-      case 0xFFE01F: // -
-        if (is_alarm_setting && hhmm_idx > 0)
-          hhmm[--hhmm_idx] = 0;
-        break;
-      case 0xFFC23D: // Play
-        is_alarm_setting = !is_alarm_setting;
-        is_alarm_set = !is_alarm_set;
-        break;
-      case 0xFF6897: // 0
-      case 0xFF30CF: // 1
-      case 0xFF18E7: // 2
-      case 0xFF7A85: // 3
-      case 0xFF10EF: // 4
-      case 0xFF38C7: // 5
-      case 0xFF5AA5: // 6
-      case 0xFF42BD: // 7
-      case 0xFF4AB5: // 8
-      case 0xFF52AD: // 9
-        if (is_alarm_setting && hhmm_idx < 4) {
-          int input_num = result_to_int(result.value);
-          hhmm[hhmm_idx++] = input_num;
+    if (irrecv.decode(&result)) {
+        switch (result.value) {
+            case 0xFF906F:  // EQ
+                is_alarm_setting = !is_alarm_setting;
+                break;
+            case 0xFFE01F:  // -
+                if (is_alarm_setting && hhmm_idx > 0)
+                    hhmm[--hhmm_idx] = 0;
+                break;
+            case 0xFFC23D:  // Play
+                is_alarm_setting = !is_alarm_setting;
+                is_alarm_set = !is_alarm_set;
+                break;
+            case 0xFF6897:  // 0
+            case 0xFF30CF:  // 1
+            case 0xFF18E7:  // 2
+            case 0xFF7A85:  // 3
+            case 0xFF10EF:  // 4
+            case 0xFF38C7:  // 5
+            case 0xFF5AA5:  // 6
+            case 0xFF42BD:  // 7
+            case 0xFF4AB5:  // 8
+            case 0xFF52AD:  // 9
+                if (is_alarm_setting && hhmm_idx < 4) {
+                    int input_num = result_to_int(result.value);
+                    hhmm[hhmm_idx++] = input_num;
+                }
+                break;
+            default:
+                break;
         }
-        break;
-      default:
-        break;
+
+        /* for (int i = 0; i < 4; i++) Serial.print(hhmm[i]);
+        Serial.println(); */
+
+        delay(250);
+        irrecv.resume();
     }
-
-    /* for (int i = 0; i < 4; i++) Serial.print(hhmm[i]);
-    Serial.println(); */
-
-    delay(250);
-    irrecv.resume();
-  }
 }
 
 void update_time() {
-  static unsigned long elapsed_time;
+    static unsigned long elapsed_time;
 
-  if (cur_time.hour == -1 && cur_time.minute == -1) {
-    while(!Serial.available());
-    delay(250); // 모든 바이트가 전송되었을 때 까지 기다리기
+    if (cur_time.hour == -1 && cur_time.minute == -1) {
+        while (!Serial.available());
+        delay(250);  // 모든 바이트가 전송되었을 때 까지 기다리기
 
-    /* String raw = Serial.readString();
-    cur_time.hour = raw.substring(0, 2).toInt();
-    cur_time.minute = raw.substring(2, 4).toInt(); */
-    cur_time.hour = Serial.parseInt();
-    cur_time.minute = Serial.parseInt();
-    
-    #ifdef DEBUG
-      if (cur_time.hour < 10) Serial.print('0');
-      Serial.print(cur_time.hour);
-      Serial.print(':');
-      if (cur_time.minute < 10) Serial.print('0');
-      Serial.println(cur_time.minute);
-    #endif
-  } else {
-    if (millis() - elapsed_time >= 60000) {
-      cur_time.minute++;
-      if (cur_time.minute == 60) {
-        cur_time.minute = 0;
-        cur_time.hour++;
-        if (cur_time.hour == 24)
-          cur_time.hour = 0;
-      }
-      elapsed_time = millis();
+        /* String raw = Serial.readString();
+        cur_time.hour = raw.substring(0, 2).toInt();
+        cur_time.minute = raw.substring(2, 4).toInt(); */
+        cur_time.hour = Serial.parseInt();
+        cur_time.minute = Serial.parseInt();
+
+        #ifdef DEBUG_MODE
+            if (cur_time.hour < 10) Serial.print('0');
+            Serial.print(cur_time.hour);
+            Serial.print(':');
+            if (cur_time.minute < 10) Serial.print('0');
+            Serial.println(cur_time.minute);
+        #endif
+    } else {
+        if (millis() - elapsed_time >= 60000) {
+            cur_time.minute++;
+            if (cur_time.minute == 60) {
+                cur_time.minute = 0;
+                cur_time.hour++;
+                if (cur_time.hour == 24)
+                    cur_time.hour = 0;
+            }
+            elapsed_time = millis();
+        }
     }
-  }
 }
 
 bool check_alarm() {
 }
 
 void water() {
-  digitalWrite(AA, HIGH);
-  digitalWrite(AB, LOW);
-  delay(3000);
-  digitalWrite(AA, LOW);
-  digitalWrite(AB, LOW);
-  delay(3000);
+    digitalWrite(AA, HIGH);
+    digitalWrite(AB, LOW);
+    delay(3000);
+    digitalWrite(AA, LOW);
+    digitalWrite(AB, LOW);
+    delay(3000);
 }
 
 void setup() {
-  Serial.begin(9600);
-  Serial.setTimeout(1);
+    Serial.begin(9600);
+    Serial.setTimeout(1);
 
-  irrecv = IRrecv(recvPin);
-  irrecv.enableIRIn();
+    irrecv = IRrecv(recvPin);
+    irrecv.enableIRIn();
 
-  pinMode(AA, OUTPUT);
-  pinMode(AB, OUTPUT);
+    pinMode(AA, OUTPUT);
+    pinMode(AB, OUTPUT);
 
-  pinMode(led_pin, OUTPUT);
+    pinMode(led_pin, OUTPUT);
 }
 
 void loop() {
-  update_time();
+    update_time();
 
-  set_alarm();
-  if (is_alarm_set) check_alarm();
+    set_alarm();
+    if (is_alarm_set) check_alarm();
 }
